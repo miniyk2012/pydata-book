0d085f60381bc1412f4c1cf82627ee6cce3ac4c7
Thomas Young <yk_ecust_2007@163.com>
2019-01-19 21:13:19 +0800

    [feature] 20190119 chapter03 part1

---
+++ cprof_example.py
@@ -0,0 +1,26 @@
+import numpy as np
+from numpy.linalg import eigvals
+
+@profile
+def run_experiment(niter=100):
+    K = 100
+    results = []
+    for _ in xrange(niter):
+        mat = np.random.randn(K, K)
+        max_eigenvalue = np.abs(eigvals(mat)).max()
+        results.append(max_eigenvalue)
+
+    return results
+
+def fun():
+    print(1)
+    print(2)
+    print(3)
+
+
+some_results = run_experiment()
+print('Laregest one we saw %s' % np.max(some_results))
+print(111224)
+ax = 10
+
+
